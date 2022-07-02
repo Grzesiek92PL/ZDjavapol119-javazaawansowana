508347eb86c8455173c2cc9c02e8c745548ea10f
Piotr Andrzejak <pandrzejak93@gmail.com>
2022-07-02 12:33:01 +0200

    inner classes

---
+++ src/com/sda/classes/Main.java
@@ -0,0 +1,29 @@
+package com.sda.classes;
+
+import com.sda.classes.inner.Outer;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Outer.NestedStatic nestedStatic = new Outer.NestedStatic();
+
+        //wywołanie na rzecz obiektu
+        nestedStatic.saySomething_1();
+        //wywołanie na rzecz klasy
+        Outer.NestedStatic.saySomething_2();
+
+
+
+        Outer outer = new Outer();
+        Outer.Inner inner_1 = outer.new Inner();
+
+        Outer.Inner inner_2 = new Outer().new Inner();
+
+
+
+
+
+
+    }
+}
